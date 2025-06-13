700c848e7c28e89e04c06c0dac0c7973137cdd9a
Miguel Almeida <miguel.almeida@sou.inteli.edu.br>
2025-06-13 08:32:31 -0300

    feat: adiciona criação de dados exemplos ao executar o script de criação das tabelas

--- migrations/init.sql
+++ migrations/init.sql
@@ -5,6 +5,12 @@ CREATE TABLE IF NOT EXISTS usuario (
   email VARCHAR (50) NOT NULL
 );
 
+-- Dados de exemplo: Usuário
+INSERT INTO usuario (nome, email) VALUES
+('Enzo Silva', 'enzo@exemplo.com'),
+('Mariana Castro', 'mariana@exemplo.com');
+
+
 -- Criação Tabela "Sala"
 CREATE TABLE IF NOT EXISTS sala (
   id_sala SERIAL PRIMARY KEY,
@@ -13,6 +19,12 @@ CREATE TABLE IF NOT EXISTS sala (
   capacidade INTEGER,
   disponivel BOOLEAN
 );
+
+-- Dados de exemplo: Sala
+INSERT INTO sala (numero, localizacao, capacidade, disponivel) VALUES
+(101, 'Bloco A - Térreo', 20, TRUE),
+(202, 'Bloco B - 2º Andar', 15, TRUE);
+
 
 -- Criação Tabela "Reserva"
 CREATE TABLE IF NOT EXISTS reserva (
@@ -25,3 +37,8 @@ CREATE TABLE IF NOT EXISTS reserva (
   FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
   FOREIGN KEY (id_sala) REFERENCES sala(id_sala)
 );
+
+-- Dados de exemplo: Reserva
+INSERT INTO reserva (id_usuario, id_sala, reservado_em, data_inicio, data_fim) VALUES
+(1, 1, CURRENT_TIMESTAMP, '2025-06-14 09:00:00', '2025-06-14 10:00:00'),
+(2, 2, CURRENT_TIMESTAMP, '2025-06-14 14:00:00', '2025-06-14 15:30:00');
